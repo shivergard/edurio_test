88ce6b113a03bc13c40b351b08618927686d8913
Shivergard <shivergard@greenbox.lv>
2023-04-27 17:21:28 +0300

    added delete test for answer

--- src/tests/Unit/AnswerTest.php
+++ src/tests/Unit/AnswerTest.php
@@ -28,4 +28,54 @@ class AnswerTest extends TestCase
             $this->assertEquals(5, $answer->order);
             $this->assertEquals($question->id, $answer->question()->get()[0]->id);
         }
+
+        /** @test */
+        public function it_can_update_a_answer()
+        {
+            $survey = $this->createTestSurvey();
+            $question = $this->createTestQuestion($survey);
+
+            $answer = Answer::create([
+                'question_id' => $question->id,
+                'value' => "TEST Answer",
+                'order' => 5
+            ]);
+
+            $this->assertInstanceOf(Answer::class, $answer);
+
+            $newQuestion = $this->createTestQuestion($survey);
+
+            $answer->update([
+                'question_id' => $newQuestion->id,
+                'value' => "TEST Answer Update",
+                'order' => 7
+            ]);
+
+            $this->assertEquals($newQuestion->id, $answer->question_id);
+            $this->assertEquals('TEST Answer Update', $answer->value);
+            $this->assertEquals(7, $answer->order);
+            $this->assertEquals($newQuestion->id, $answer->question()->get()[0]->id);
+        }
+
+        /** @test */
+        public function it_can_delete_a_answer()
+        {
+            $survey = $this->createTestSurvey();
+            $question = $this->createTestQuestion($survey);
+
+            $answer = Answer::create([
+                'question_id' => $question->id,
+                'value' => "TEST Answer",
+                'order' => 5
+            ]);
+
+            $this->assertInstanceOf(Answer::class, $answer);
+
+            $answerId = $answer->id;
+
+            $answer->delete();
+
+            $answerResponse = Answer::where('id', $answerId);
+            $this->assertEquals(0, $answerResponse->count());
+        }
 }
