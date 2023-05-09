66e71ad964abc53d199cb3182a659973fe00c4a4
Shivergard <shivergard@greenbox.lv>
2023-05-09 20:32:55 +0300

    finalize QuestionsController test implementation

--- src/tests/Feature/QuestionsControllerTest.php
+++ src/tests/Feature/QuestionsControllerTest.php
@@ -79,17 +79,91 @@ class QuestionsControllerTest extends TestCase
 
     public function test_index_returns_404_for_inactive_survey()
     {
-
+        // Create a survey
+        $survey = Survey::create([
+            'title' => 'Test Survey',
+            'description' => 'This is a test survey.',
+            'status' => false,
+        ]);
+    
+        // Create some questions for the survey
+        $questions = [
+            [
+                'text' => 'Question 1',
+                'type' => 1,
+            ],
+            [
+                'text' => 'Question 2',
+                'type' => 2,
+            ],
+            [
+                'text' => 'Question 3',
+                'type' => 1,
+            ],
+        ];
+        foreach ($questions as $question) {
+            $survey->questions()->create($question);
+        }
+    
+        // Send a GET request to the index endpoint with the survey ID
+        $response = $this->get('/surveys/' . $survey->id . '/questions');
+    
+        // Assert that the response status code is 404
+        $response->assertStatus(404);
     }
+    
 
     public function test_store_creates_new_question_for_active_survey()
     {
-
+        // Create a new survey
+        $survey = Survey::create([
+            'title' => 'New Survey',
+            'description' => 'This is a new survey.',
+            'status' => 1,
+        ]);
+    
+        // Make a POST request to the store endpoint with new question data
+        $response = $this->post("/api/surveys/{$survey->id}/questions", [
+            'text' => 'What is your favorite color?',
+            'type' => 1,
+        ]);
+    
+        // Assert that the response is successful
+        $response->assertStatus(Response::HTTP_CREATED);
+    
+        // Assert that the question was created and saved to the database
+        $this->assertDatabaseHas('questions', [
+            'survey_id' => $survey->id,
+            'text' => 'What is your favorite color?',
+            'type' => 1,
+        ]);
     }
+    
 
     public function test_store_returns_404_for_inactive_survey()
     {
-
-    }
+        // Create a new inactive survey
+        $survey = Survey::create([
+            'title' => 'New Survey',
+            'description' => 'This is a new survey.',
+            'status' => 0, // inactive
+        ]);
+    
+        // Make a POST request to the store endpoint with new question data
+        $response = $this->post("/surveys/{$survey->id}/questions", [
+            'text' => 'What is your favorite color?',
+            'type' => 1,
+        ]);
+    
+        // Assert that the response is not successful and returns a 404 status code
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    
+        // Assert that the question was not created and saved to the database
+        $this->assertDatabaseMissing('questions', [
+            'survey_id' => $survey->id,
+            'text' => 'What is your favorite color?',
+            'type' => 1,
+        ]);
+    }    
 
 }
